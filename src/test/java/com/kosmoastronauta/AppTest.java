96a530a6ab6da4298929a08c5a05422673a1b223
Mateusz Gil <mateusz_gil@outlook.com>
2019-08-26 11:35:33 +0200

    Created first simple test checking title

--- src/test/java/com/kosmoastronauta/AppTest.java
+++ src/test/java/com/kosmoastronauta/AppTest.java
@@ -1,5 +1,35 @@
 package com.kosmoastronauta;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AppTest
 {
+    public WebDriver driver;
+
+    @BeforeAll
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void setupTest() {
+         driver= new ChromeDriver();
+
+    }
+
+    @AfterEach
+    public void teardown() {
+        driver.close();
+    }
+
+    @Test
+    public void test() {
+        driver.get("http://localhost:4200/");
+        Assertions.assertEquals("AngularLA",driver.getTitle());
+
+
+    }
 }
