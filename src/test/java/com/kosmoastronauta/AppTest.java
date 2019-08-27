533a22abafd9abbf78d5cc734ef39d827bf8a45a
Mateusz Gil <mateusz_gil@outlook.com>
2019-08-27 10:13:45 +0200

    added test for creating member without any data and commented closing driver which coused errors

--- src/test/java/com/kosmoastronauta/AppTest.java
+++ src/test/java/com/kosmoastronauta/AppTest.java
@@ -2,11 +2,17 @@ package com.kosmoastronauta;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.UnhandledAlertException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AppTest
 {
+    public static final String WEB = "http://localhost:4200/";
     public WebDriver driver;
 
     @BeforeAll
@@ -17,17 +23,37 @@ public class AppTest
     @BeforeEach
     public void setupTest() {
          driver= new ChromeDriver();
-
     }
 
     @AfterEach
     public void teardown() {
-        driver.close();
+       // driver.close();
     }
 
     @Test
-    public void test() {
-        driver.get("http://localhost:4200/");
+    public void checkingTitleOfWebsite() {
+        driver.get(WEB);
         Assertions.assertEquals("AngularLA",driver.getTitle());
+    }
+
+    @Test
+    public void addingMemberWithoutAnyData() throws InterruptedException
+    {
+        String message = "";
+        driver.get(WEB);
+        driver.findElement(By.id("addMemberNav")).click();
+        driver.findElement(By.id("submitMember")).click();
+        Thread.sleep(1000);
+
+        try
+        {
+            Alert alert;
+            alert = driver.switchTo().alert();
+            message = alert.getText();
+        }catch(UnhandledAlertException e)
+        {
+            System.out.println(message);
+            Assertions.assertEquals(message, "Error while creating new member");
+        }
     }
 }
